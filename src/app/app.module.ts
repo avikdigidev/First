e73bdc23c9b3f92485a8296fce77fe2afa773967
Prakash Shelke <avikdigidev@gmail.com>
2020-07-24 11:22:05 +0530

    Dependency Injection Added
    logic is implemented in course.service
    any dependency which is needed by the component in the decalaration
      //must be added in the providers. Else dependency injection wont work and we get error: No provider for coursesService
    //Singleton instance of the provider is created which can be used by multiple instances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {CoursesComponent} from './courses.component';
-import { CourseComponent } from './course/course.component';
+import {CourseComponent} from './course/course.component';
+import {CoursesService} from './courses.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,11 @@ import { CourseComponent } from './course/course.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+//any dependency which is needed by the component in the decalaration
+  //must be added in the providers. Else dependency injection wont work and we get error: No provider for coursesService
+//Singleton instance of the provider is created which can be used by multiple instances
+  providers: [CoursesService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {CoursesService} from './courses.service';
 
 @Component({
   selector: 'courses',
@@ -18,7 +19,14 @@ import {Component} from '@angular/core';
 })
 export class CoursesComponent {
   title = 'List of Courses';
-  courses = ['course1', 'course2', 'course3'];
+  courses;
+//instead of creating an object of CourseService we pass it as dependency
+  //to constructor and let angular instatiate the object
+  //this way we have decoupled the logic from the component.
+  //Dependency Injection
+  constructor(service: CoursesService) {
+    this.courses = service.getCourses();
+  }
 
   getTitle() {
     return this.title;
