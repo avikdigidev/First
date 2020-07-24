cc11fceabad5a63f4b2cbcbb9a5baf74c78751eb
Prakash Shelke <avikdigidev@gmail.com>
2020-07-24 10:42:43 +0530

    Updated code: Interpolation added
    Calling the method in template instead of variable

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,12 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'courses',
-  template: '<h2>Courses</h2>'
+  //calling the method instead of the variable is called Interpolation
+  template: '<h2>{{getTitle()}}</h2>'
 })
-export class CoursesComponent{
-
+export class CoursesComponent {
+  title = 'List of Courses';
+  getTitle() {
+    return this.title;
+  }
 }
