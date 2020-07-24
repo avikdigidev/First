c832228e5b69bf6e6c9fe30e60cebb096f3fe113
Prakash Shelke <avikdigidev@gmail.com>
2020-07-24 10:54:52 +0530

    Updated code: applied directives
     changing from ' to ` to make it multiline and readable code
     foreach loop in angular
     * before ng whenever we plan to change the structure of DOM

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,10 +3,23 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'courses',
   //calling the method instead of the variable is called Interpolation
-  template: '<h2>{{getTitle()}}</h2>'
+  //template: '<h2>{{getTitle()}}</h2>'
+  //applied directives
+  //changing from ' to ` to make it multiline and readable code
+  //foreach loop in angular
+  //* before ng whenever we plan to change the structure of DOM
+  template: `
+    <h2>{{getTitle()}}</h2>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{course}}
+      </li>
+    </ul>`
 })
 export class CoursesComponent {
   title = 'List of Courses';
+  courses = ['course1', 'course2', 'course3'];
+
   getTitle() {
     return this.title;
   }
