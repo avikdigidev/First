53463422c3ed17a56c137d7dc838923da1f4aff4
Prakash Shelke <avikdigidev@gmail.com>
2020-08-17 01:20:12 +0530

    Added Class binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -29,6 +29,22 @@ import {CoursesService} from './courses.service';
   //   <img src="{{ imageUrl }}" />
   //   <img [src] = "imageUrl"/>
   // `
+
+  //Class binding
+  //There are times that we may want to add additional classes to an element based on some condition.
+//For example here I want to apply the active class on this button based on the state of the underlying component.
+//Here is to specify the name of the target class active.
+//Now we bind this to a field or a property in our class or component.
+//So let me define a field here. isActive is set to true.
+//
+//If I come back here and change the value of this field to false and save; then the active class is gone.
+//So we refer to this syntax as class binding.
+//If this condition here evaluates to TRUE this target class will be added to this element.
+//Otherwise if it's false and this class exists on the element will be removed.
+//
+//Also I want to clarify that I separated this class binding from the actual class attribute because I
+//want to keep these two classes btn and btn-primary seperate at all times
+//I only want to add the active class dynamically based on some condition.
   template: `
     <img [src]="imageUrl"/>
     <table>
@@ -36,12 +52,13 @@ import {CoursesService} from './courses.service';
         <td [attr.colspan] = "colSpan"></td>
       </tr>
     </table>
-    <button class = "btn btn-primary">Save</button>
+    <button class = "btn btn-primary" [class.active]="isActive">Save</button>
     `
 })
 export class CoursesComponent {
+  isActive = true;
   title = 'List of Courses';
-  imageUrl = 'https://intellij-support.jetbrains.com/hc/user_images/hN1OQky0rbTS306ES7mp1g.jpeg';
+  imageUrl = 'https://res.cloudinary.com/practicaldev/image/fetch/s--StRkI7Ze--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://codesandtags.github.io/blog/static/0c42bdee6c2a7e213cacc2b33ac3039c/a0304/hero.webp';
     colSpan = 2;
   // courses;
 // instead of creating an object of CourseService we pass it as dependency
