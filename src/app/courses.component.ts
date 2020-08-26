fce63f76a58b0e9156dfa58fa70cebfd3aa755a1
Prakash Shelke <avikdigidev@gmail.com>
2020-08-26 22:49:24 +0530

    Added event handling
    We also have event binding which we use to handle events raised from the DOM keystrokes mouse movements
    clicks and so on.
    So here we have a simple button and we want to handle the click event of this button.
    So instead of square brackets use parenthesis and here we are at the name of the event like click then
    we bind this to a method in our component.
    So let's call this on save.
    Now here I create this method on say and simply log a message in a console console that log button was
    clicked.
    So here's our button click.
    Now let's take a look at the cons. OK here's the message now sometimes we need to get access to the
    event object that was raised in the event handler for example with mouse movements the event object
    will tell us that x and y position if you want to get access to that event object we need to add that
    as a parameter here.
    So dollar event and then when calling this method you also pass dollar event dollar event object is
    something known to angular.
    Now in this case we are dealing with a DOM object.
    So this dom or event object will represent a dom event a standard Dom event that you have seen in vanilla
    Javascript.
    
    So I can or on Save method.
    Let's lock this event dollar event same in the browser.
    So one more time click OK here's our daughter event objects all these properties you see here are part
    of the standard Dom event objects so we have the movement x and y offset X and Y and many other properties.
    Now all the dummy events bubble up the DOM tree unless a handler along the way prevents further bubbling.
    This is just a standard event propagation mechanism in DOM and it's nothing specific about angular.
    For example let me wrap this button with a div like this.
    
    Now this Div I also want to handle the click event so practices click and I want to bind this to a
    different method.
    So let's call this on div clicked
    let's add this method here on div click.
    And here I want to log a different message div was clicked.
    Now let's see what happens.
    So when we click this button we get two messages in the console.
    The first one is the handler for the Click event of the button.
    And the second is from the handler of the Click event of the div.
    So this is what we call event bubbling and event bubbles up the DOM tree.
    
    So to extend this example if we have another div or another element that contained this div and we handle
    the click event on that element our event object will bubble love and hate that target handler.
    How can we stop event bubbling.
    Well here in on say we can call $event.stopPropagation()
    
    Again this is a standard method we have seen before in vanilla Javascript.
    So when we call this this event will not bubble up.
    In other words it's not going to hit the second handler a straight save like in the browser.
    Click now look in the console.
    We only have one message that's coming from the handler of the Click event of the button.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -30,34 +30,34 @@ import {CoursesService} from './courses.service';
   //   <img [src] = "imageUrl"/>
   // `
 
-  //Class binding
-  //There are times that we may want to add additional classes to an element based on some condition.
-//For example here I want to apply the active class on this button based on the state of the underlying component.
-//Here is to specify the name of the target class active.
-//Now we bind this to a field or a property in our class or component.
-//So let me define a field here. isActive is set to true.
+  // Class binding
+  // There are times that we may want to add additional classes to an element based on some condition.
+// For example here I want to apply the active class on this button based on the state of the underlying component.
+// Here is to specify the name of the target class active.
+// Now we bind this to a field or a property in our class or component.
+// So let me define a field here. isActive is set to true.
 //
-//If I come back here and change the value of this field to false and save; then the active class is gone.
-//So we refer to this syntax as class binding.
-//If this condition here evaluates to TRUE this target class will be added to this element.
-//Otherwise if it's false and this class exists on the element will be removed.
+// If I come back here and change the value of this field to false and save; then the active class is gone.
+// So we refer to this syntax as class binding.
+// If this condition here evaluates to TRUE this target class will be added to this element.
+// Otherwise if it's false and this class exists on the element will be removed.
 //
-//Also I want to clarify that I separated this class binding from the actual class attribute because I
-//want to keep these two classes btn and btn-primary seperate at all times
-//I only want to add the active class dynamically based on some condition.
-  //We also have style binding which is again a variation of property binding very similar to class binding.
-//So back to our example if I want to apply some inline styles to this ban based on some condition I can
-//type something like this.
-//So we start with the square brackets now type style and then we can add any properties of this style
-//object in done.
-//For example we've got background color now for the complete list of all the properties in this style
-//object.
-//You can simply search for Dom style object properties.
-//If you scroll down here the first link it's DML Darmstadt object on W3 schools and this page and this
-//table you can see all the properties available in the style object like we set this to an expression.
-//So if is active is true we want the background color to be blue and not that here I'm putting blue between
-//single quotes otherwise I want the background color to be white.
-//So this is what we call style binding.
+// Also I want to clarify that I separated this class binding from the actual class attribute because I
+// want to keep these two classes btn and btn-primary seperate at all times
+// I only want to add the active class dynamically based on some condition.
+  // We also have style binding which is again a variation of property binding very similar to class binding.
+// So back to our example if I want to apply some inline styles to this ban based on some condition I can
+// type something like this.
+// So we start with the square brackets now type style and then we can add any properties of this style
+// object in done.
+// For example we've got background color now for the complete list of all the properties in this style
+// object.
+// You can simply search for Dom style object properties.
+// If you scroll down here the first link it's DML Darmstadt object on W3 schools and this page and this
+// table you can see all the properties available in the style object like we set this to an expression.
+// So if is active is true we want the background color to be blue and not that here I'm putting blue between
+// single quotes otherwise I want the background color to be white.
+// So this is what we call style binding.
   template: `
     <img [src]="imageUrl"/>
     <table>
@@ -65,7 +65,9 @@ import {CoursesService} from './courses.service';
         <td [attr.colspan] = "colSpan"></td>
       </tr>
     </table>
-    <button [style.backgroundColor]= "isActive ? 'blue' : 'red' "class = "btn btn-primary" [class.active]="isActive">Save</button>
+    <div (click)="onDivClick($event)">
+    <button (click)="onSave($event)"[style.backgroundColor]= "isActive ? 'blue' : 'red' "class = "btn btn-primary" [class.active]="isActive">Save</button>
+    </div>
     `
 })
 export class CoursesComponent {
@@ -73,6 +75,15 @@ export class CoursesComponent {
   title = 'List of Courses';
   imageUrl = 'https://res.cloudinary.com/practicaldev/image/fetch/s--StRkI7Ze--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://codesandtags.github.io/blog/static/0c42bdee6c2a7e213cacc2b33ac3039c/a0304/hero.webp';
     colSpan = 2;
+
+  onSave($event){
+    $event.stopPropagation();
+    console.log('button was clicked', $event);
+  }
+  onDivClick($event){
+
+    console.log('div button was clicked', $event);
+  }
   // courses;
 // instead of creating an object of CourseService we pass it as dependency
   // to constructor and let angular instatiate the object
